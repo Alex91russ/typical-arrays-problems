7af00c458478e5aea24f4b7a3f31fe370e5ca993
Aleksandr Kochetkov <k_alex91@mail.ru>
2020-09-29 20:06:17 +0400

    feat: complite task typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  const sortArray = array.sort((a, b) => a - b);
+  return sortArray[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  const sortArray = array.sort((a, b) => b - a);
+  return sortArray[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  const sum = array.reduce((acc, value) => acc + value, 0);
+  return sum / array.length;
 }
